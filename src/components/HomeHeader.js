21f347a688f31175d466288045216818d2937ca7
IsacHolstad <wisac22@gmail.com>
2023-03-14 13:20:51 +0100

    h2 Pitch yourself ✅

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -16,15 +16,14 @@ const HomeHeader = () => {
             <Nav>
                 <span>
                     <img src="/header_img.jpg" alt="Header" />
-                    <h1 className="my-1 text-5xl text-center">GRADUATION SHOWCASE</h1>
+                    <h1 className="my-1 text-5xl text-center font-semibold">GRADUATION SHOWCASE</h1>
                     <div className="mx-auto">
-                        <img
-                            className="mx-auto w-5 h-5 my-8 object-contain"
-                            src="/Noroff_shield 1.png"
-                        />
+                        <img className="mx-auto mt-16 w-8 h-14 my-8 object-contain" src="/Noroff_shield 1.png"/>
                     </div>
                 </span>
-
+                <span>
+                    <h2 className="text-center mt-64 font-bold text-4xl">Pitch Yourself</h2>
+                </span>
             </Nav>
 
         </>
