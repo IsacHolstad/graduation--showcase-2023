6f661687885c44c503ab1bf835892d4380d97be9
IsacHolstad <wisac22@gmail.com>
2023-03-14 13:13:44 +0100

    noroff logo to pubilc

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -14,8 +14,17 @@ const HomeHeader = () => {
     return (
         <>
             <Nav>
-                <img src="/header_img.jpg" alt="Header" />
-                <h1 className="my-1 text-5xl text-center">Graduation showcase</h1>
+                <span>
+                    <img src="/header_img.jpg" alt="Header" />
+                    <h1 className="my-1 text-5xl text-center">GRADUATION SHOWCASE</h1>
+                    <div className="mx-auto">
+                        <img
+                            className="mx-auto w-5 h-5 my-8 object-contain"
+                            src="/Noroff_shield 1.png"
+                        />
+                    </div>
+                </span>
+
             </Nav>
 
         </>
